f1badd3f2cee58ef186f02d4cbba82abfdbd2a47
2tle <iam@2tle.io>
2022-01-26 00:32:00 +0900

    update: eslint and bodyparser

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,17 @@
-import express, {Request, Response, NextFunction} from "express";
-import morgan from "morgan";
+import express from 'express';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
 const app = express();
 
-app.use(morgan("common"));
-
+app.use(bodyParser.json());
+app.use(
+    bodyParser.urlencoded({
+        extended: false,
+    })
+);
+app.use(morgan('common'));
 
 app.listen(process.env.PORT || 3000, () => {
-    console.log("Server is listening port 3000!");
+    console.log('Server is listening port 3000!');
 });
