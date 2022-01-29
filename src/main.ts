f65f94dc284f58064c83749c7a6f28f25f1a492c
KHJcode <kimstudswcode4485@gmail.com>
2022-01-29 14:32:36 +0900

    edit: main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import App from './app';
 const { app } = new App();
 app.listen(app.get('port'), () => {
-    console.log('Server is listening port ' + app.get('port').toString());
+    console.log(`Server is listening port ${app.get('port')}.`);
 });
