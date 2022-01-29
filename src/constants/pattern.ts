172742d9a91f2bb88a1ec8dddf0359aebf275726
2tle <iam@2tle.io>
2022-01-30 05:38:33 +0900

    update: sunrin student and teacher(seoul) email regex

--- src/constants/pattern.ts
+++ src/constants/pattern.ts
@@ -1,3 +1,11 @@
 export const SUNRIN_EMAIL_PATTERN = new RegExp(
     '^[A-Za-z0-9._%+-]+@sunrint.hs.kr$'
 );
+
+export const SUNRIN_STUDENT_EMAIL_PATTERN = new RegExp(
+    '^\\d{2}sunrin\\d{3}@sunrint.hs.kr'
+);
+
+export const TEACHER_SEOUL_EDUCATION_EMAIL_PATTERN = new RegExp(
+    '^[A-Za-z0-9._%+-]+@sen.go.kr$'
+);
