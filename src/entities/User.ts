4f93020dcec962ac298b306dfc5c4103f806d7a2
2tle <iam@2tle.io>
2022-01-30 02:02:45 +0900

    create: model User.ts

---
+++ src/entities/User.ts
@@ -0,0 +1,34 @@
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: number;
+
+    @Column({ type: 'string', nullable: false, unique: true })
+    email: string;
+
+    @Column({ type: 'string', nullable: true, unique: true })
+    teacherEmail: string;
+
+    @Column({ type: 'string', nullable: false })
+    password: string;
+
+    @Column({ type: 'string', nullable: false, unique: true, length: 10 })
+    username: string;
+
+    @Column({ type: 'string', nullable: false })
+    department: string;
+
+    @Column({ type: 'int', nullable: true })
+    grade: number;
+
+    @Column({ type: 'int', nullable: true })
+    class: number;
+
+    @Column({ type: 'int', nullable: true })
+    number: number;
+
+    @Column({ type: 'int', default: 0, nullable: false })
+    accountType: number;
+}
