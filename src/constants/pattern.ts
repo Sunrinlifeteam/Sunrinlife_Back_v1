cab4c18cf58f363bac95d0b641d400ada9b50ace
2tle <iam@2tle.io>
2022-01-30 20:47:34 +0900

    update: regex name

--- src/constants/pattern.ts
+++ src/constants/pattern.ts
@@ -6,6 +6,6 @@ export const SUNRIN_STUDENT_EMAIL_PATTERN = new RegExp(
     '^\\d{2}sunrin\\d{3}@sunrint.hs.kr'
 );
 
-export const TEACHER_SEOUL_EDUCATION_EMAIL_PATTERN = new RegExp(
+export const SEOUL_EDU_EMAIL_PATTERN = new RegExp(
     '^[A-Za-z0-9._%+-]+@sen.go.kr$'
 );
