79bad9c1be3501666ee4f11f7178466e0653e944
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-02-08 18:33:09 +0900

    fix: fix tests

--- test/services/intranetNotice.test.ts
+++ test/services/intranetNotice.test.ts
@@ -1,20 +1,43 @@
-import { Attachment, IntranetNotice } from '../../src/services/intranetNotice'
+/* eslint-disable prettier/prettier */
+import { IntranetNotice } from '../../src/services/intranetNotice';
+import { Attachment } from '../../src/types/attachment';
 
 test.each([
-    [1,'notice title','notice content',new Date(),new Date(),[]],
-    [2,'notice titlee','notice contentee',new Date(2022, 2, 3),new Date(2022, 2, 3),[]],
-    [3,'notice titleee','notice contenteee',new Date(2005, 1, 21),new Date(2005, 1, 21),[new Attachment("notice.txt", Buffer.from("Testt"), "text/plain")]]
-])("", (id, title, content, created, updated, attachment) => {
+    [1, 'notice title', 'notice content', new Date(), new Date(), []],
+    [
+        2,
+        'notice titlee',
+        'notice contentee',
+        new Date(2022, 2, 3),
+        new Date(2022, 2, 3),
+        [],
+    ],
+    [
+        3,
+        'notice titleee',
+        'notice contenteee',
+        new Date(2005, 1, 21),
+        new Date(2005, 1, 21),
+        [new Attachment('notice.txt', Buffer.from('Testt'), 'text/plain')],
+    ],
+])('', (id, title, content, created, updated, attachment) => {
     const original = { id, title, content, created, updated, attachment };
-    const obj = new IntranetNotice(id, title, content, created, updated, attachment)
-    expect(obj.id).toBe(id)
-    expect(obj.title).toBe(title)
-    expect(obj.content).toBe(content)
-    expect(obj.created).toBe(created)
-    expect(obj.updated).toBe(updated)
-    expect(obj.attachment).toBe(attachment)
-    expect(obj.toObject()).toEqual(original)
-    expect(obj.toJSON()).toBe(JSON.stringify(original))
-    expect(IntranetNotice.fromObject(original)).toEqual(original)
-    expect(IntranetNotice.fromJSON(JSON.stringify(original))).toEqual(original)
-})
+    const obj = new IntranetNotice(
+        id,
+        title,
+        content,
+        created,
+        updated,
+        attachment
+    );
+    expect(obj.id).toBe(id);
+    expect(obj.title).toBe(title);
+    expect(obj.content).toBe(content);
+    expect(obj.created).toBe(created);
+    expect(obj.updated).toBe(updated);
+    expect(obj.attachment).toBe(attachment);
+    expect(obj.toObject()).toEqual(original);
+    expect(obj.toJSON()).toBe(JSON.stringify(original));
+    expect(IntranetNotice.fromObject(original)).toEqual(original);
+    expect(IntranetNotice.fromJSON(JSON.stringify(original))).toEqual(original);
+});
--- test/services/schedule.test.ts
+++ test/services/schedule.test.ts
@@ -1,18 +1,31 @@
-import { Attachment, Schedule } from '../../src/services/schedule'
+/* eslint-disable prettier/prettier */
+import { Schedule } from '../../src/services/schedule';
+import { Attachment } from '../../src/types/attachment';
 
 test.each([
-    [new Date(), "today schedule", "nothing", []],
-    [new Date(2022, 2, 3), "schedule 1", "nothing", []],
-    [new Date(1999, 12, 30), "schedule 2", "attachment", [new Attachment("school.txt", Buffer.from("Sunrin Internet High School"), "text/plain")]]
-])("", (date, title, body, attachment) => {
+    [new Date(), 'today schedule', 'nothing', []],
+    [new Date(2022, 2, 3), 'schedule 1', 'nothing', []],
+    [
+        new Date(1999, 12, 30),
+        'schedule 2',
+        'attachment',
+        [
+            new Attachment(
+                'school.txt',
+                Buffer.from('Sunrin Internet High School'),
+                'text/plain'
+            ),
+        ],
+    ],
+])('', (date, title, body, attachment) => {
     const original = { date, title, body, attachment };
-    const obj = new Schedule(date, title, body, attachment)
-    expect(obj.date).toBe(date)
-    expect(obj.title).toBe(title)
-    expect(obj.body).toBe(body)
-    expect(obj.attachment).toBe(attachment)
-    expect(obj.toObject()).toEqual(original)
-    expect(obj.toJSON()).toBe(JSON.stringify(original))
-    expect(Schedule.fromObject(original)).toEqual(original)
-    expect(Schedule.fromJSON(JSON.stringify(original))).toEqual(original)
-})
+    const obj = new Schedule(date, title, body, attachment);
+    expect(obj.date).toBe(date);
+    expect(obj.title).toBe(title);
+    expect(obj.body).toBe(body);
+    expect(obj.attachment).toBe(attachment);
+    expect(obj.toObject()).toEqual(original);
+    expect(obj.toJSON()).toBe(JSON.stringify(original));
+    expect(Schedule.fromObject(original)).toEqual(original);
+    expect(Schedule.fromJSON(JSON.stringify(original))).toEqual(original);
+});
