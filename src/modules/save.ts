a02848802481a00d9aa03683b6ee8be3a3c0fab4
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-02-09 15:54:29 +0900

    edit: UPLOAD_PATH default to /tmp

--- src/modules/save.ts
+++ src/modules/save.ts
@@ -4,8 +4,8 @@ import { Attachment } from '../models/attachment';
 import { MD5, SHA256 } from './hash';
 
 export function SaveFile(filename: string, data: string, mimetype: string) {
-    const UPLOAD_PATH = process.env.UPLOAD_PATH;
-    if (UPLOAD_PATH == undefined) throw Error('UPLOAD_PATH is empty');
+    const UPLOAD_PATH = process.env.UPLOAD_PATH || '/tmp';
+    //if (UPLOAD_PATH == undefined) throw Error('UPLOAD_PATH is empty');
     writeFileSync(path.resolve(UPLOAD_PATH, SHA256(data)), data);
     return new Attachment(
         filename,
