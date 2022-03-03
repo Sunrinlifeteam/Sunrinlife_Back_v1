de139f079d0e2fc32e9acac60c3f67a68681200e
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-03-03 19:57:29 +0900

    create: upload file filter

--- src/modules/upload.ts
+++ src/modules/upload.ts
@@ -1,7 +1,19 @@
 import multer from 'multer';
+import { extname } from 'path';
+
+const allowExtension = ['.png', '.jpg', '.gif', '.jpeg'];
+
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
         cb(null, process.env.UPLOAD_TEMP_PATH || './tmp');
     },
 });
-export default multer({ storage: storage });
+export default multer({
+    storage: storage,
+    fileFilter: (req, file, callback) => {
+        var ext = extname(file.originalname);
+        if (!allowExtension.includes(ext))
+            return callback(new Error('Not allowed file extension'));
+        callback(null, true);
+    },
+});
