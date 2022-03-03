4effb260ddae0717f7fb2159e2e8fefb00580589
Yun Do Hyun <yundohyun050121@gmail.com>
2022-03-03 19:12:33 +0900

    edit: Notices entity, model, service

--- src/models/schoolNotice.ts
+++ src/models/schoolNotice.ts
@@ -1,4 +1,7 @@
 import { Attachment, IAttachment } from '../models/attachment';
+import { SchoolNoticeData } from '../entities/SchoolNotice';
+import { AttachmentRecord as AttachmentRecord } from '../entities/Attachment';
+import { getConnection } from 'typeorm';
 
 export interface ISchoolNotice {
     id: number;
@@ -11,6 +14,7 @@ export interface ISchoolNotice {
 export interface INoticeBody {
     title: string;
     content: string;
+    created: Date;
     attachment: Array<IAttachment>;
 }
 
@@ -18,6 +22,7 @@ export interface INoticeBodyWithID {
     id: number;
     title: string;
     content: string;
+    created: Date;
     attachment: Array<IAttachment>;
 }
 
@@ -55,6 +60,46 @@ export class SchoolNotice implements ISchoolNotice {
         return JSON.stringify(this.toObject());
     }
 
+    async toActiveRecord(): Promise<SchoolNoticeData> {
+        let record = new SchoolNoticeData();
+        record.id = +this.id;
+        record.created = new Date(this.created)
+        record.title = this.title;
+        record.content = this.content;
+        record.attachment = await Promise.all(
+            this.attachment.map((x) =>
+                Attachment.fromObject(x).toActiveRecord()
+            )
+        );
+        return await getConnection().manager.save(record);
+    }
+
+    static fromActiveRecord(record: SchoolNoticeData): SchoolNotice {
+        return SchoolNotice.fromObject({
+            id: +record.id,
+            created: new Date(record.created),
+            title: record.title,
+            content: record.content,
+            attachment: (record.attachment || []).map((x) =>
+                Attachment.fromActiveRecord(x)
+            ),
+        });
+    }
+
+    static async fromBody(data: INoticeBodyWithID): Promise<SchoolNotice> {
+        return SchoolNotice.fromObject({
+            id: +data.id,
+            created: new Date(data.created),
+            title: data.title,
+            content: data.content,
+            attachment: (
+                await getConnection().manager.findByIds(
+                    AttachmentRecord,
+                    data.attachment
+                )
+            ).map((x) => Attachment.fromActiveRecord(x)),
+        });
+    }
     static fromObject(data: ISchoolNotice): SchoolNotice {
         return new SchoolNotice(
             data.id,
