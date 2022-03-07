8c2fe5e074be24ea9f2eb0f357dd8120441a8fda
rmagur1203 <rmagur1203@naver.com>
2022-03-08 07:59:02 +0900

    create: comci module

---
+++ src/modules/comci/fetch.ts
@@ -0,0 +1,9 @@
+import axios from "axios"
+
+export async function searchSchool(host: string, searchPath: string, search: string){
+    const url = `http://${host}/${searchPath}${search}`
+    let res = await axios({
+        url,
+    });
+    return res.data;
+}
---
+++ src/modules/comci/test.ts
@@ -0,0 +1,8 @@
+import { Parse, ParseServer, searchSchool } from ".";
+
+(async () => {
+    const server = await ParseServer();
+    if (!server) return;
+    const data = await Parse(server.host);
+    console.log(data);
+})();
