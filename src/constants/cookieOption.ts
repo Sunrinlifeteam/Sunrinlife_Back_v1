17f7a62f069b17ac8a45304418f1a9829db323b4
KHJcode <kimstudswcode4485@gmail.com>
2022-03-12 18:09:29 +0900

    bugfix: cookie option

--- src/constants/cookieOption.ts
+++ src/constants/cookieOption.ts
@@ -1,7 +1,7 @@
 import { isProduction } from './isProduction';
 
 export const REFRESH_TOKEN_COOKIE_OPTION = {
-    ...(isProduction ? { domain: process.env.SERVICE_DOMAIN } : {}),
+    domain: process.env.SERVICE_DOMAIN,
     httpOnly: isProduction,
     secure: isProduction,
     maxAge: 2592000000,
