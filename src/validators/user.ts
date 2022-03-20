77c36a181fbd02241e6a09c7c466b6828b3964a8
KHJcode <kimstudswcode4485@gmail.com>
2022-03-20 18:07:02 +0900

    edit: update user joi object

--- src/validators/user.ts
+++ src/validators/user.ts
@@ -2,10 +2,10 @@ import { Segments } from 'celebrate';
 import Joi from 'joi';
 
 const updateUserJoiObject = Joi.object({
-    description: Joi.string().max(150),
-    image: Joi.string(),
-    githubLink: Joi.string().max(200),
-    clubInfo: Joi.number(),
+    description: Joi.string().max(150).optional(),
+    image: Joi.string().optional(),
+    githubLink: Joi.string().max(200).optional(),
+    clubInfo: Joi.number().optional(),
 });
 
 export const updateUserValidator = {
