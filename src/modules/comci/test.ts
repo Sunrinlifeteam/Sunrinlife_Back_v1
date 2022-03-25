e33b7162e60fd9d24d199047c8d235f740aec84d
rmagur1203 <rmagur1203@naver.com>
2022-03-26 00:11:34 +0900

    Delete test.ts

--- src/modules/comci/test.ts
+++
@@ -1,8 +0,0 @@
-import { Parse, ParseServer, searchSchool } from '.';
-
-(async () => {
-    const server = await ParseServer();
-    if (!server) return;
-    const data = await Parse(server.host);
-    console.log(data);
-})();
