0d2dd43ea69886002454c15ca44db04c1ab65f50
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-04-01 16:45:11 +0900

    edit: load controller

--- src/loaders/route.ts
+++ src/loaders/route.ts
@@ -6,10 +6,12 @@ import { UploadController } from '../controllers/upload';
 import { ScheduleController } from '../controllers/schedule';
 import { ClubInfoController } from '../controllers/clubInfo';
 import { NoticeController } from '../controllers/notice';
+import { UserController } from '../controllers/user';
 
 export default async (app: express.Application) => {
     attachControllers(app, [UploadController]);
     attachControllers(app, [AuthController]);
+    attachControllers(app, [UserController]);
     attachControllers(app, [UserScheduleController, ScheduleController]);
     attachControllers(app, [NoticeController]);
     attachControllers(app, [ClubInfoController]);
