71a372294ffb913762e4f1fccf1a3318737fe9e1
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-04-05 19:50:12 +0900

    create: anonymous board, board

---
+++ src/entities/BlindBoard.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@decorators/di';
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinTable,
+    ManyToMany,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { AttachmentEntity } from './Attachment';
+
+@Entity('blindboard')
+@Injectable()
+export class BlindBoard {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false })
+    title: string;
+
+    @Column({ nullable: false })
+    content: string;
+
+    @CreateDateColumn()
+    created: Date;
+
+    @UpdateDateColumn()
+    updated: Date;
+
+    @ManyToMany(() => AttachmentEntity)
+    @JoinTable()
+    attachment: AttachmentEntity[];
+}
---
+++ src/entities/Board.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@decorators/di';
+import {
+    Column,
+    CreateDateColumn,
+    Entity,
+    JoinColumn,
+    JoinTable,
+    ManyToMany,
+    ManyToOne,
+    PrimaryGeneratedColumn,
+    UpdateDateColumn,
+} from 'typeorm';
+import { AttachmentEntity } from './Attachment';
+import { UserEntity } from './User';
+
+@Entity('board')
+@Injectable()
+export class Board {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ nullable: false })
+    title: string;
+
+    @Column({ nullable: false })
+    content: string;
+
+    @CreateDateColumn()
+    created: Date;
+
+    @UpdateDateColumn()
+    updated: Date;
+
+    @ManyToOne(() => UserEntity)
+    @JoinColumn()
+    author: UserEntity;
+
+    @ManyToMany(() => AttachmentEntity)
+    @JoinTable()
+    attachment: AttachmentEntity[];
+}
