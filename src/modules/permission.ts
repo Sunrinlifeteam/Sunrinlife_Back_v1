692b36c951915a9cad39efc966d021e6390e5d7f
rmagur1203 <rmagur1203@naver.com>
2022-06-13 02:31:24 +0900

    preview with github pages (#73)
    * edit: generate typedoc.json & apply hierarchy theme
    
    * change: add typedoc output option
    
    * change: apply logger
    
    * fix: anonymous board like api bug fix
    
    * edit: can redirect to desired page

--- src/modules/permission.ts
+++ src/modules/permission.ts
@@ -1,11 +1,12 @@
 import { Request, RequestHandler, Response } from 'express';
 import HttpStatusCode from '../constants/HttpStatusCode';
+import logger from './logger';
 
 export function permission(roles: number[]): RequestHandler {
     return function (req: Request, res: Response, next: any) {
         if (!req.user) return res.sendStatus(HttpStatusCode.UNAUTHORIZED);
         for (const role of roles) {
-            console.log(req.user, role);
+            logger.debug(req.user, role);
             if (!(req.user.role & role))
                 return res.sendStatus(HttpStatusCode.UNAUTHORIZED);
         }
