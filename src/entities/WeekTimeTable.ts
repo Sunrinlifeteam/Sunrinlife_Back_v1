966c1fe6ef4a31588756ee0fdb92e3a46b7c66ea
Hyunju Yang <sw4019@naver.com>
2022-06-16 03:45:47 +0000

    updaate: week timetable entity

--- src/entities/WeekTimeTable.ts
+++ src/entities/WeekTimeTable.ts
@@ -2,6 +2,16 @@ import { Injectable } from '@decorators/di';
 import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, JoinTable } from 'typeorm';
 import { TimeTableEntity } from './TimeTable';
 
+export const WEEK_TIMETABLE_SELECT: (keyof WeekTimeTableEntity)[] = [
+    'sun',
+    'mon',
+    'tue',
+    'wed',
+    'thu',
+    'fri',
+    'sat',
+];
+
 @Entity('week_timetable')
 @Injectable()
 export class WeekTimeTableEntity {
