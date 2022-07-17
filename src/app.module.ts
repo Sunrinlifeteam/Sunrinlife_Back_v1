bee4b3d8bafd43b904baead19de7d70b7e76209e
rmagur1203 <41236839+rmagur1203@users.noreply.github.com>
2022-07-18 02:14:02 +0900

    feat: import hello module for example

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { HelloModule } from './shared/modules/hello/hello.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    HelloModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: [`.env`],
+    }),
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
+import { Logger } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const config = app.get(ConfigService);
+  await app.listen(config.get<number>('PORT'));
+  Logger.log(
+    `Server running on http://localhost:${config.get<number>('PORT')}`,
+    'Bootstrap',
+  );
 }
 bootstrap();
